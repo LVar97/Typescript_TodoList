ea8c3534033afb50a59642284207c9d81a132f75
Varvara Karelina <letkovaa@mail.ru>
2022-02-17 02:46:17 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { AboutPage } from './pages/AboutPage';
 import { TodosPage } from './pages/TodosPage';
 
-
-
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar/>
       <div className="container">
         <Routes>
-          <Route element={<AboutPage/>} path="/"/>
+          <Route path="/" element={<AboutPage/>}/>
           <Route element={<TodosPage/>} path="/about"/>
         </Routes>
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
