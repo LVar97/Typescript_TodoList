d16065055f9f3ec18f012a103bc10bf401d464be
Varvara Karelina <letkovaa@mail.ru>
2022-02-17 12:31:54 +0300

    build

--- src/hook/useActions.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as TodosActionCreators from "../store/acrtion-creators/todos";
-
-export const useActions = () => {
-	const dispatch = useDispatch();
-	return bindActionCreators(TodosActionCreators, dispatch);
-};
--- src/store/redusers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { todosReducer } from "./todosReducer";
-
-export const rootReducer = combineReducers({
-	todo: todosReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>
